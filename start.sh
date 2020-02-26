552bed297acda16329df9effe346d8c8b2dcb97e
tom <tom@sapletta.com>
2020-02-26 20:14:28 +0100

    update scripts

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-npm i -g npm-upgrade
+sudo apt-get update
+sudo apt-get install git-core curl build-essential openssl libssl-dev
+sudo apt install nodejs -y && apt install npm -y
+curl -L https://npmjs.org/install.sh | sudo sh
+##&& npm i -g npm-upgrade
 npm i
 npm audit fix
 npm i -g bower
